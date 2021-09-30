4502b52d9db414c98ffd6214696ef54ea1de12d4
“T3sultan” <incognito.soul011@gmail.com>
2021-10-01 00:33:21 +0600

    rouder setup components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
+
+import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="*">
+           <NotFoundPage></NotFoundPage>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
