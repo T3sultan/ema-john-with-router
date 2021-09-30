f2ce76f7d01aad7671db9e45e0a294e4c61f4c04
“T3sultan” <incognito.soul011@gmail.com>
2021-10-01 02:07:58 +0600

    cart product

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
 
 const OrderReview = () => {
     const [products] = useProducts();
+    const [cart] = useCart(products)
     return (
-        <div>
-            <h2>{products.length}</h2>
+        <div className="shop-container">
+            <div className='product-container'>
+                 {
+                     
+                 }
+
+
+            </div>
+            <div className="cart-container">
+                <Cart
+                    cart={cart}
+                ></Cart>
+
+            </div>
         </div>
     );
 };
