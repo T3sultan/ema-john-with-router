82cd8691e7a27680f985536f313ad1e0731a8ee0
“T3sultan” <incognito.soul011@gmail.com>
2021-10-01 11:05:39 +0600

    item remove

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -2,16 +2,28 @@ import React from 'react';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const OrderReview = () => {
     const [products] = useProducts();
-    const [cart] = useCart(products)
+    const [cart, ssetCart] = useCart(products);
+    const handleRemove = key => {
+        const newCart = cart.filter(product => product.key !== key);
+        ssetCart(newCart);
+    }
+
     return (
         <div className="shop-container">
             <div className='product-container'>
-                 {
-                     
-                 }
+                {
+                    cart.map(product => <ReviewItem
+                        product={product}
+                        key={product.key}
+                        handleRemove={handleRemove}
+                    >
+
+                    </ReviewItem>)
+                }
 
 
             </div>
--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -25,6 +25,6 @@ const useCart = products => {
 
 
     }, [products])
-    return [cart];
+    return [cart,setCart];
 }
 export default useCart;
